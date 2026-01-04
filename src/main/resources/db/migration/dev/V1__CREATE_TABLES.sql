7fabd41e0f06c8667b02e76c2a141c192787a15f
esaldana <eder.11.joel@gmail.com>
2026-01-04 14:55:58 -0500

    feat: Correccion de base de datos.

--- src/main/resources/db/migration/dev/V1__CREATE_TABLES.sql
+++ src/main/resources/db/migration/dev/V1__CREATE_TABLES.sql
@@ -6,10 +6,18 @@
 
 -- Tabla users
 CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    role VARCHAR(20),
-    password VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP );
+           id BIGSERIAL PRIMARY KEY,
+           name VARCHAR(100) NOT NULL,
+           email VARCHAR(100) NOT NULL,
+           role VARCHAR(20),
+           password VARCHAR(100) NOT NULL,
+           created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+           updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+           CONSTRAINT chk_name_not_empty CHECK (LENGTH(TRIM(name)) > 0),
+           CONSTRAINT chk_email_format CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
+    );
+
+-- Comentarios
+COMMENT ON TABLE users IS 'Usuarios del sistema - DB en Docker';
+COMMENT ON COLUMN users.id IS 'Identificador único del usuario';
--- src/main/resources/db/migration/dev/V3__INSERT_DATA.sql
+++ src/main/resources/db/migration/dev/V3__INSERT_DATA.sql
@@ -2,10 +2,10 @@
 -- Migration: V3__INSERT_DATA.sql
 -- ============================================
 
-INSERT INTO users (name, email, role) VALUES
+INSERT INTO users (name, email, role, password) VALUES
 ('Juan Pérez', 'juan.perez@example.com', 'ADMIN', '12345'),
-('María García', 'maria.garcia@example.com', 'USER', ''),
-('Carlos López', 'carlos.lopez@example.com', 'USER', ''),
-('Ana Torres', 'ana.torres@example.com', 'USER', ''),
-('Luis Fernández', 'luis.fernandez@example.com', 'USER', ''),
-('Roberto Sánchez', 'roberto.sanchez@example.com', 'USER', '');
+('María García', 'maria.garcia@example.com', 'USER', '123'),
+('Carlos López', 'carlos.lopez@example.com', 'USER', '123'),
+('Ana Torres', 'ana.torres@example.com', 'USER', '123'),
+('Luis Fernández', 'luis.fernandez@example.com', 'USER', '123'),
+('Roberto Sánchez', 'roberto.sanchez@example.com', 'USER', '123');
